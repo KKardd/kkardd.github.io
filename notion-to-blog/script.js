64e3f658619c1ae8ee64f49d153e06aed6ec3ba5
KKardd <wlstjd4013@gmail.com>
2024-07-16 14:44:57 +0900

    refactor: refine n2m string value

--- notion-to-blog/script.js
+++ notion-to-blog/script.js
@@ -3,6 +3,7 @@ const {NotionToMarkdown} = require("notion-to-md");
 const getKey = require("./get-secret-notion-key.js");
 const getPostStyle = require("./post-style.constant.js");
 const fs = require("fs");
+const path = require("path");
 
 const secreyKey = getKey();
 
@@ -15,21 +16,26 @@ const n2m = new NotionToMarkdown({notionClient: notion});
 
 (async () => {
     // 제목 설정
-    const title = "Menu Orders";
+    const title = "Test Post";
     // 페이지 ID 설정
-    const mdblocks = await n2m.pageToMarkdown("cfc3f346f7b7402288353d7d7474afe7");
+    const mdblocks = await n2m.pageToMarkdown("52a907bed6144f5d8fe562a7da813609");
 
     const date = await getDate();
-    const mdString = n2m.toMarkdownString(mdblocks);
+    let mdString = n2m.toMarkdownString(mdblocks);
+    mdString.parent = mdString.parent.replace(/\n\n/g, "\n");
     const refinedPost = getPostStyle(title);
 
-    fs.writeFile(`../study/_posts/${date}-${title}.md`, refinedPost + mdString.parent, (err) => {
-        if (err) {
-            console.error("파일을 저장하는 동안 오류가 발생했습니다.", err);
-        } else {
-            console.log("파일이 성공적으로 저장되었습니다.");
+    fs.writeFile(
+        path.resolve(__dirname + "/..") + `/study/_posts/${date}-${title}.md`,
+        refinedPost + mdString.parent,
+        (err) => {
+            if (err) {
+                console.error("파일을 저장하는 동안 오류가 발생했습니다.", err);
+            } else {
+                console.log("파일이 성공적으로 저장되었습니다.");
+            }
         }
-    });
+    );
 })();
 
 async function getDate() {
